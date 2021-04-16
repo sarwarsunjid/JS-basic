c3faa9b33f60e7e483136d5e5900203c56f1b168
unknown <sarwarsunjid@gmail.com>
2021-04-16 21:14:34 +0600

    commenting on code

---
+++ lec-1/Js-Comment-your-code/main.js
@@ -0,0 +1,17 @@
+//single line comment 
+//Single line comments start with //.any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+var a = 5; // declaring a variable with value
+console.log(a);
+
+//Multiline comment starts with /* */
+//Multi-line comments start with /* and end with */. Any text between /* and */ will be ignored by JavaScript.
+
+/*
+Declaring a variable
+and printing its value
+*/
+
+var a=0;
+var a = a + 10;
+console.log(a);
