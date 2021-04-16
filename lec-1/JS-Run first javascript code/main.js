babc83293dac139c0097c8e6a34d4be8f90f2a42
unknown <sarwarsunjid@gmail.com>
2021-04-16 18:01:00 +0600

    Running first javascript code
    Tested this file using quokka.js

---
+++ lec-1/JS-Run first javascript code/main.js
@@ -0,0 +1,7 @@
+//Running first javascript code
+console.log('hello world')
+
+//some other example 
+console.log('My Name is Sarwar');
+console.log('I love my country');
+console.log('My age is 28');
