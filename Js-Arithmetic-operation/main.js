c7669c217e3fce0a853e4d89535b7d14bdd19c54
Sarwar Jahan Bhuiyan <sarwarsunjid@gmail.com>
2021-04-18 02:27:55 +0600

    Increment, Decrement, Remainder added

--- Js-Arithmetic-operation/main.js
+++ Js-Arithmetic-operation/main.js
@@ -1,4 +1,4 @@
-//Addition two number
+//Addition of two number
 var a = 10;
 var b = 100;
 
@@ -7,7 +7,7 @@ var sum = a + b;
 console.log("Result of summation is "+sum);
 
 
-//Substract of two number 
+//Substraction of two number 
 var first = 50;
 var last = 10;
 
@@ -34,3 +34,34 @@ var z = 2;
 var div = x / z;
 
 console.log("Result of Division is "+div);
+
+//Increment value 
+var inc = 100;
+
+inc++;
+
+console.log("Increment value "+inc);
+
+//Decrement value 
+var inc = 100;
+
+inc--;
+
+console.log("Increment value "+inc);
+
+// multiply decimal number 
+var myDecimal = 0.02 * 2;
+
+console.log(myDecimal);
+
+// divide decimal number
+var quotient = 4.4 /2.2
+console.log(quotient);
+
+
+//check remainder 
+var remainder;
+remainder = 11 % 3;
+
+console.log(remainder);
+
