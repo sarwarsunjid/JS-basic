6fe8b9d4931ceac0184a390a91f7edeef0c5553f
Sarwar Jahan Bhuiyan <sarwarsunjid@gmail.com>
2021-04-18 05:30:40 +0600

    Augmented(Addition, substraction, multiplication, division))

--- Js-Augmented-Arithmetic-operation/main.js
+++ Js-Augmented-Arithmetic-operation/main.js
@@ -42,3 +42,47 @@ c -= 2; //modified previous line
 console.log(+a,+b,+c);
 
 
+
+//Augmented multiplication
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+
+//check code below 
+
+//a = a + 12;
+a *= 6; //modified previous line 
+
+//b = b + 9;
+b *= 5; //modified previous line 
+
+//c = c + 7;
+c *= 2; //modified previous line 
+
+//check output
+console.log(+a,+b,+c);
+
+
+
+//Augmented division
+
+var a = 48;
+var b =110;
+var c = 33;
+
+
+//check code below 
+
+//a = a + 12;
+a /= 12; //modified previous line 
+
+//b = b + 9;
+b /= 5; //modified previous line 
+
+//c = c + 7;
+c /= 11; //modified previous line 
+
+//check output
+console.log(+a,+b,+c);
