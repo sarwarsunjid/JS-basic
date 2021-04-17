70ad766a0fd820735afa0320c8d6453b22e3dfe7
Sarwar Jahan Bhuiyan <sarwarsunjid@gmail.com>
2021-04-18 02:43:07 +0600

    Augmented Addition, Augmented Substraction

---
+++ Js-Augmented-Arithmetic-operation/main.js
@@ -0,0 +1,44 @@
+//Augmented addition
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+
+//check code below 
+
+//a = a + 12;
+a += 12; //modified previous line 
+
+//b = b + 9;
+b += 9; //modified previous line 
+
+//c = c + 7;
+c += 7; //modified previous line 
+
+//check output
+console.log(+a,+b,+c);
+
+
+//Augmented substraction
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+
+//check code below 
+
+//a = a + 12;
+a -= 6; //modified previous line 
+
+//b = b + 9;
+b -= 5; //modified previous line 
+
+//c = c + 7;
+c -= 2; //modified previous line 
+
+//check output
+console.log(+a,+b,+c);
+
+
