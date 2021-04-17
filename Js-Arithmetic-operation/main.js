7228651aee1d134150e5bf0fdf8170893370cb12
Sarwar Jahan Bhuiyan <sarwarsunjid@gmail.com>
2021-04-17 22:14:37 +0600

    Basic Arithmetic Operation

---
+++ Js-Arithmetic-operation/main.js
@@ -0,0 +1,36 @@
+//Addition two number
+var a = 10;
+var b = 100;
+
+var sum = a + b;
+
+console.log("Result of summation is "+sum);
+
+
+//Substract of two number 
+var first = 50;
+var last = 10;
+
+var difference = first - last;
+
+console.log("Result of Substraction is "+difference);
+
+
+//Multiplication of two number 
+var c = 10;
+
+var d = 10;
+
+var mul = c * d;
+
+console.log("Result of multiplication is "+mul);
+
+
+//Division of two number 
+var x = 10;
+
+var z = 2;
+
+var div = x / z;
+
+console.log("Result of Division is "+div);
